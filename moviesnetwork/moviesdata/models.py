2872e85bf87945f89973d5d861a0b093e52047fb
Danilo Freitas <dsurviver@gmail.com>
2015-12-09 20:29:43 -0300

    #6 - Configuring project for MongoDB

--- moviesnetwork/moviesdata/models.py
+++ moviesnetwork/moviesdata/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from djangotoolbox.fields import ListField, EmbeddedModelField
 
 
 class Actor(models.Model):
 
     name = models.CharField(blank=False, max_length=50, unique=True)
+    casting = ListField(EmbeddedModelField('Cast'))
 
     class Meta:
         verbose_name = "Actor"
@@ -16,6 +18,7 @@ class Actor(models.Model):
 class Movie(models.Model):
 
     name = models.CharField(blank=False, max_length=50, unique=True)
+    cast = ListField(EmbeddedModelField('Cast'))
 
     class Meta:
         verbose_name = "Movie"
@@ -28,8 +31,6 @@ class Movie(models.Model):
 class Cast(models.Model):
 
     character = models.CharField(blank=False, max_length=50)
-    movie = models.ForeignKey(Movie, related_name='cast')
-    actor = models.ForeignKey(Actor, related_name='casting')
 
     class Meta:
         verbose_name = "Cast"
