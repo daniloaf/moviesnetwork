422fbd624856345de79f454a8a397c7202d75d9f
Danilo Freitas <dsurviver@gmail.com>
2016-01-04 22:30:38 -0300

    #19 - Creating cache for actors

--- moviesnetwork/moviesdata/services.py
+++ moviesnetwork/moviesdata/services.py
@@ -1,6 +1,12 @@
 import imdb
+from django.core.cache import cache
 
 from data import Actor
+
+
+CACHE_EXPIRATION_TIME = 60 * 60 * 24 * 7  # One week
+ACTOR_CACHE_KEY_PREFIX = 'actor_'
+MOVIE_CACHE_KEY_PREFIX = 'movie_'
 
 
 def search_actor(name):
@@ -17,7 +23,11 @@ def get_actor_info(actor_id):
     """
     Returns Actor with full info from IMDb.
     """
-    imdb_access = imdb.IMDb()
-    result = imdb_access.get_person(actor_id)
-    actor = Actor.create(result, True) if result.data else None
+    cache_key = ACTOR_CACHE_KEY_PREFIX + str(actor_id)
+    actor = cache.get(cache_key)
+    if not actor:
+        imdb_access = imdb.IMDb()
+        result = imdb_access.get_person(actor_id)
+        actor = Actor.create(result, True) if result.data else None
+        cache.set(cache_key, actor, CACHE_EXPIRATION_TIME)
     return actor
